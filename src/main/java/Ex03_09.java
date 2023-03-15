10602adc5593b10f9b19c79d2ef004aaacbad536
LeeHwapyeong <hp0918kr@gmail.com>
2023-03-15 11:28:33 +0900

    Ex03_09

--- src/main/java/Ex03_09.java
+++ src/main/java/Ex03_09.java
@@ -1,15 +1,18 @@
 public class Ex03_09 {
     public static void main(String[] args) {
-        int a, b;
-        float c, d;
+        int a, b, c, d;
+
+        a = 100 + 100;
+        b = a + 100;
+        c = a + b - 100;
+
+        d = a + b + c;
+
+        System.out.printf("a, b, c, d 의 값 ==> %d, %d, %d, %d \n", a, b, c, d);
 
         a = 100;
-        b = a;
+        a = a + 200;
 
-        c = 111.1f;
-        d = c;
-
-        System.out.printf("a, b의 값 ==> %d , %d \n", a, b);
-        System.out.printf("c, d의 값 ==> %5.1f , %5.1f \n", c, d);
+        System.out.printf("a의 값 ==> %d \n", a);
     }
 }
