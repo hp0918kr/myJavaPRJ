3fdcf81ea18a0753ca0ef69b23249ecc65ceef9e
LeeHwapyeong <hp0918kr@gmail.com>
2023-03-15 09:40:16 +0900

    수정2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,5 +7,9 @@ public class Hello {
 
         System.out.println("제 이름은 이화평입니다.");
 
+        System.out.println("제 가족은 부모님, 큰형, 누나, 작은형, 그리고 막내인 저 이렇게 6명입니다.");
+
+        System.out.println("누가 뭐라해도 멋진 가족들입니다.");
+
     }
 }
