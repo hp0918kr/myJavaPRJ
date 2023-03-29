f67977982aacaa0cbfb406f48c5d548e1530cb34
LeeHwapyeong <hp0918kr@gmail.com>
2023-03-29 10:19:40 +0900

    Ex05_08 중첩 if문 수정

---
+++ src/main/java/Ex05_08.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ex05_08 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.println("점수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if (a >= 90) {
+            System.out.println("A");
+
+        } else {
+            if (a >= 80) {
+                System.out.println("B");
+
+            } else {
+                if (a >= 70) {
+                    System.out.println("C");
+                } else {
+                    if (a >= 60) {
+                        System.out.println("D");
+                    } else {
+                        if (a >= 50) {
+                            System.out.println("F");
+                        }
+                        System.out.println(" 학점 입니다.\n");
+                    }
+                }
+            }
+        }
+    }
+}
