5cb0e7256748071e5048c8b7f2b4103e28e32fcf
LeeHwapyeong <hp0918kr@gmail.com>
2023-04-05 11:27:10 +0900

    Ex08_04 배열의 초기화 length 추가 + bcd

--- src/main/java/Ex08_04.java
+++ src/main/java/Ex08_04.java
@@ -14,14 +14,18 @@ public class Ex08_04 {
             System.out.printf("aa[%d]==>%d\t", i, aa[i]);
         System.out.printf("\n");
 
+        System.out.println("bb 몇개? : "+ bb.length);
         for (i = 0; i < 3; i++)
             System.out.printf("bb[%d]==>%d\t", i, bb[i]);
         System.out.printf("\n");
 
+
+        System.out.println("cc 몇개? : "+ cc.length);
         for (i = 0; i < 2; i++)
             System.out.printf("cc[%d]==>%d\t", i, cc[i]);
         System.out.printf("\n");
 
+        System.out.println("dd 몇개? : "+ dd.length);
         for (i = 0; i < 1; i++)
             System.out.printf("dd[%d]==>%d\t", i, dd[i]);
         System.out.printf("\n");
