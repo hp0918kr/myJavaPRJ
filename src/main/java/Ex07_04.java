243596e8034169f50b2ed6d10594c623b5a21293
LeeHwapyeong <hp0918kr@gmail.com>
2023-04-05 09:50:51 +0900

    Ex07_04 while문 무한루프 계산기

---
+++ src/main/java/Ex07_04.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex07_04 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        char ch;
+
+        while (true){
+            System.out.printf("계산할 첫번째 수를 입력 : ");
+            a = sc.nextInt();
+            System.out.printf("계산할 두번째 수를 입력 : ");
+            b = sc.nextInt();
+            System.out.printf("계산할 연산자를 입력하세요 : ");
+            ch = (char) System.in.read();
+
+            switch (ch) {
+                case '+':
+                    System.out.printf("%d + %d = %d 입니다. \n", a, b, a + b); break;
+                case '-':
+                    System.out.printf("%d - %d = %d 입니다. \n", a, b, a - b); break;
+                case '*':
+                    System.out.printf("%d * %d = %d 입니다. \n", a, b, a * b); break;
+                case '/':
+                    System.out.printf("%d / %d = %d 입니다. \n", a, b, a / b); break;
+                case '%':
+                    System.out.printf("%d %% %d = %d 입니다. \n", a, b, a % b); break;
+                default:
+                    System.out.printf("연산자를 잘못 입력했습니다. \n");
+            }
+        }
+    }
+}
