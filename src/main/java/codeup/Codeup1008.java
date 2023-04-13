078197c9f2f2c25dc73d5242379016b510b8a3ce
LeeHwapyeong <hp0918kr@gmail.com>
2023-04-14 08:57:51 +0900

    코드업 1008

---
+++ src/main/java/codeup/Codeup1008.java
@@ -0,0 +1,9 @@
+package codeup;
+
+public class Codeup1008 {
+    public static void main(String[] args) {
+        System.out.printf("\u250c" + "\u252c" + "\u2510" + "\n");
+        System.out.printf("\u251c"+"\u253c"+"\u2524"+"\n");
+        System.out.printf("\u2514" + "\u2534" + "\u2518" + "\n");
+    }
+}
