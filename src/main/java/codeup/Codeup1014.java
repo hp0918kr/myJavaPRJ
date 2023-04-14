0df2cb41717467c072abea47ac7718d607e31e6a
LeeHwapyeong <hp0918kr@gmail.com>
2023-04-14 12:00:39 +0900

    코드업 1014

---
+++ src/main/java/codeup/Codeup1014.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a, b;
+        a = sc.next().charAt(0);
+        b = sc.next().charAt(0);
+        System.out.printf("%c %c", b, a );
+    }
+}
