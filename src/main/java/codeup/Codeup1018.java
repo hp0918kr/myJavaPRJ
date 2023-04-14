5b92a39b4f023dfb0922d23b5196470bf6577e67
LeeHwapyeong <hp0918kr@gmail.com>
2023-04-14 12:01:16 +0900

    코드업 1018

---
+++ src/main/java/codeup/Codeup1018.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b= sc.nextInt();
+        System.out.printf("%d : %d", a, b); // :기준으로 두 수가 각 변수에 저장
+    }
+}
