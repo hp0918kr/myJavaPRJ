9add8d6c1ee2fdee93a4a75c961c17644852c220
LeeHwapyeong <hp0918kr@gmail.com>
2023-04-25 12:24:54 +0900

    2023.04.25 퀴즈0329 for문으로 구구단 가로찍기

---
+++ src/main/java/Quiz0329.java
@@ -0,0 +1,12 @@
+package src.main.java;
+
+public class Quiz0329 {
+    public static void main(String[] args) {
+        for (int k = 1; k < 10; k++) {
+            for (int i = 2; i < 10; i++) {
+                System.out.print(i + "x" + k + "=" + i * k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
