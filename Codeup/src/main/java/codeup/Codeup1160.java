79389d8861b0e462c243100d817cd675c50f315b
LeeHwapyeong <hp0918kr@gmail.com>
2023-05-08 15:23:25 +0900

    2023.05.08 코드업 1160

---
+++ Codeup/src/main/java/codeup/Codeup1160.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup1160 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 2 != 0) { //if문에 a가 2로 나누어 떨어지지 않으면 홀수이므로 조건을 설정
+            System.out.print("oh my god");
+            }else {
+                System.out.print("enjoy");
+            }
+        }
+    }
+
